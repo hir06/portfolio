d0a0823cfff1b97a268fdd43e4c0a4d46256ba59
vishals <vishals@PRINHYLTPAP1333.local>
2019-01-18 13:18:08 +0530

    Added express

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express'),
+  path = require('path');
+const app = express();
+app.use(express.static('.dist/portfolio'));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(_dirname, './dist/portfolio/index.html'));
+});
+
+app.listen(process.env.PORT || 8080 , () => {
+  console.log('Server Started...');
+});
