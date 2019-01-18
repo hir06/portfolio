03d5b01c86ab3e948352e20fa81ba71868730da4
vishals <vishals@PRINHYLTPAP1333.local>
2019-01-18 13:48:45 +0530

    updated server

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express'),
   path = require('path');
 const app = express();
-app.use(express.static('.dist/portfolio'));
+app.use(express.static('./portfolio'));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, './dist/portfolio/index.html'));
+  res.sendFile(path.join(__dirname, './portfolio/index.html'));
 });
 
 app.listen(process.env.PORT || 8080 , () => {
